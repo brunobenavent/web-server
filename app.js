20c370306aadb6271acd33c40cffe4551bea4099
Bruno Benavent Colomo <70907703+brunobenavent@users.noreply.github.com>
2023-04-20 10:46:56 +0200

    webserver listo

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+require('dotenv').config
 const express = require('express')
+const hbs = require('hbs');
+
 const app = express()
+const port = process.env.PORT
 
-const port = 8080
-
-
+//Handlebars
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 
+//Servir contenido estático
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
@@ -14,12 +18,21 @@ app.get('/', (req, res) => {
         titulo: 'Curso de node'
     })
   })
-app.get('/generic', (req, res) => {
-  res.sendFile(__dirname + '/public/generic.html')
-})
-app.get('/elements', (req, res) => {
-    res.sendFile(__dirname + '/public/elements.html')
+
+  app.get('/elements', (req, res) => {
+    res.render('elements', {
+        nombre: 'Bruno',
+        titulo: 'Curso de node'
+    })
   })
+
+  app.get('/generic', (req, res) => {
+    res.render('generic', {
+        nombre: 'Bruno',
+        titulo: 'Curso de node'
+    })
+  })
+
 app.listen(port, ()=>{
     console.log(`Aplicación escuchando desde http://localhost:${port}`)
 })
